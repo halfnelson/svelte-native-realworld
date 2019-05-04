d1926841d8ffccbb4f3841e40cb16db405ba1452
halfnelson <dpershouse@gmail.com>
2019-05-04 13:10:08 +1000

    Article List Items

--- app/components/ArticleList.svelte
+++ app/components/ArticleList.svelte
@@ -1,19 +1,72 @@
 <listView items="{$items}" height="100%" width="100%">
-  <Template let:item>
-	<!-- Shows the list item label in the default color and style. -->
-	<label text="{item.title}" />
-  </Template>
+	<Template let:item>
+		<stackLayout orientation="vertical" class="article-item">
+      {#if item.tagList && item.tagList.length > 0}
+      <wrapLayout orientation="horizontal" class="tags" horizontalAlignment="right">
+				{#each item.tagList as tag, i}
+				<label text="{tag}" class="article-tag" />
+				{/each}
+			</wrapLayout>
+      {/if}
+      <label text="{item.title}" class="article-title" textWrap="true"/>
+      <label text="{item.description}" class="article-desc" textWrap="true"/>
+    
+      <gridLayout columns="*,auto,auto" class="article-footer">
+			  <Author col="0" author="{item.author}" date="{item.createdAt}" />
+        <label col="1" text="{item.favoritesCount > 0 ? icons['favorite'] : icons['favorite-outline']}" class="icon {item.favoritesCount > 0 ? 'favorited' : ''}" verticalAlignment="center" />
+        <label col="2" text="{item.favoritesCount}" class="favorites-count" verticalAlignment="center" />
+      </gridLayout>
+		</stackLayout>
+	</Template>
 </listView>
 
+<style>
+  .article-item {
+    padding: 15
+  }
+  .article-title {
+    font-size: 20;
+    font-weight: bold;
+    color: black;
+  }
+  .article-desc {
+    font-size: 17;
+  }
+  .article-footer {
+    margin-top: 15;
+  }
+  .tags {
+    margin-bottom: 0;
+  }
+  .article-tag {
+    color: #AAA;
+    font-size: 11;
+    padding: 1 7; 
+    margin-right: 2;
+    border-radius: 10;
+    border-color: #CCC;
+    border-width: 1; 
+  }
+  .icon {
+    font-size: 15;
+    margin-right: 5;
+  }
+
+  .icon.favorited {
+    color: black;
+  }
+</style>
 <script>
-    import { Template } from 'svelte-native/components'
-    import { onMount } from 'svelte'
-    import { ArticleStore, ArticleFilterType } from '../stores/articles'
-    
-    export let filtertype = ArticleFilterType.Global
-    export let filterparam = null
-    export let usertoken = null
+    import { Template } from "svelte-native/components";
+    import { onMount } from "svelte";
+
+    import Author from "./Author";
+    import { icons } from "../utils/icons"
+    import { ArticleStore, ArticleFilterType } from "../stores/articles";
+
+    export let filtertype = ArticleFilterType.Global;
+    export let filterparam = null;
+    export let usertoken = null;
     let items = new ArticleStore();
-    $: items.loadArticles(filtertype, filterparam, usertoken)
-	
+    $: items.loadArticles(filtertype, filterparam, usertoken);
 </script>
---
+++ app/components/Author.svelte
@@ -0,0 +1,35 @@
+<stackLayout orientation="horizontal">
+    <image class="author-image" src="{avatar_url}" stretch="aspectFill" />
+    <stackLayout orientation="vertical">
+        <label text="{avatar_name}" class="author-name"/>
+        <label text="{article_date}" class="date"/>
+    </stackLayout>
+</stackLayout>
+
+<style>
+    .author-image {
+        width: 32;
+        height: 32;
+        border-radius: 50%;
+        margin-right: 10;
+    }
+    .author-name {
+        font-size: 12;
+        color: black;
+    }
+    .date {
+        font-size: 10;
+        color: #555;
+    }
+</style>
+<script>
+
+    import { format } from 'timeago.js'
+    export let author;
+    export let date;
+
+    let avatar_url, avatar_name, article_date;
+    $: avatar_url = (author && author.image) ? author.image : "https://static.productionready.io/images/smiley-cyrus.jpg";
+    $: avatar_name = author ? author.username : "Anonymous";
+    $: article_date = date ? format(date, 'en_US') : ""
+</script>
